7c69451b78e42bee469e1a713cf5c88d1aa50c4e
mateus <mateustaroucO@gmail.com>
2021-12-04 21:20:26 -0800

    menu hamburger pronto

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,11 +3,15 @@ function abrir(){
         document.getElementById('bar-1').classList.value = 'bar-1-1'
         document.getElementById('bar-2').classList.value = 'bar-2-1'
         document.getElementById('bar-3').classList.value = 'bar-3-1'
-        //document.getElementById('conteudo').classList.value = 'conteudo-lt'
+        document.getElementById('conteudo').classList.value = 'menu-open'
+        document.getElementById('body').classList.value = 'hiden'
+        document.getElementById('menu-mobile').classList.value = 'bg-bk'
     }else{
         document.getElementById('bar-1').classList.value = 'bar-1'
         document.getElementById('bar-2').classList.value = 'bar-2'
         document.getElementById('bar-3').classList.value = 'bar-3'
-        //document.getElementById('conteudo').classList.value = 'none'
+        document.getElementById('conteudo').classList.value = 'none'
+        document.getElementById('menu-mobile').classList.value = ''
+        document.getElementById('body').classList.value = ''
     }
 }
